6c914694c8fc03fa81f1a084c6ff8bafc5ff5457
Stephen Grice <stephengrice@live.com>
2021-05-12 22:26:30 -0400

    Translate the headline!
    Very slow at this

--- scripts/open_dict.py
+++ scripts/open_dict.py
@@ -6,3 +6,4 @@ if __name__ == "__main__":
     # An example of loading with pandas with flexible whitesapce
     df = pd.read_csv("dictionaries/chinese.csv", delimiter="\s+")
     print(df)
+    breakpoint()
