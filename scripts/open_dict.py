e4f758d4e28e9a173ce3c738e716e90fe664c51b
Stephen Grice <stephengrice@live.com>
2021-05-12 22:08:45 -0400

    Add BBC top story in Chinese, dictionary, script
    Experimenting with tab separation for the Chinese dictionary

---
+++ scripts/open_dict.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+if __name__ == "__main__":
+    # An example of loading with pandas with flexible whitesapce
+    df = pd.read_csv("dictionaries/chinese.csv", delimiter="\s+")
+    print(df)
